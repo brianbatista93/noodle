14a2f8fb586c705eab521c510d5162bde3714511
Brian Batista <7143554+brianbatista93@users.noreply.github.com>
2024-06-09 12:56:15 -0300

    Setup GitHub actions (#1)
    * Code: Add .editorconfig
    
    Helps with code style
    
    Signed-off-by: Brian Batista <brianbatista93@gmail.com>
    
    * Backend: Add base entry point
    
    Signed-off-by: Brian Batista <brianbatista93@gmail.com>
    
    * Code: Add end line
    
    Signed-off-by: Brian Batista <brianbatista93@gmail.com>
    
    * Code: Add base CMake files
    
    Signed-off-by: Brian Batista <brianbatista93@gmail.com>
    
    * Code: Add gitignore
    
    Signed-off-by: Brian Batista <brianbatista93@gmail.com>
    
    * Code: Add base CI
    
    Signed-off-by: Brian Batista <brianbatista93@gmail.com>
    
    * Code: Add name to backend build job
    
    Signed-off-by: Brian Batista <brianbatista93@gmail.com>
    
    ---------
    
    Signed-off-by: Brian Batista <brianbatista93@gmail.com>

---
+++ modules/backend/main.cpp
@@ -0,0 +1,14 @@
+/* SPDX-FileCopyrightText: 2024 Noodle Authors
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later */
+
+/** \file
+ * \ingroup backend
+ */
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, Noodle!" << std::endl;
+    return 0;
+}
